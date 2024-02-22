c2df6384f205e18706a34acb64f9055fb6851dbb
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-02-22 15:55:09 +0100

    setup dsfr

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { DsfrHead } from "@codegouvfr/react-dsfr/next-appdir/DsfrHead";
+import { DsfrProvider } from "@codegouvfr/react-dsfr/next-appdir/DsfrProvider";
+import { getHtmlAttributes } from "@codegouvfr/react-dsfr/next-appdir/getHtmlAttributes";
+import { StartDsfr } from "./StartDsfr";
+import { defaultColorScheme } from "./defaultColorScheme";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Pass'Sport - Accueil",
@@ -14,9 +17,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  //NOTE: The lang parameter is optional and defaults to "fr"
+  const lang = "fr";
   return (
-    <html lang="fr">
-      <body className={inter.className}>{children}</body>
+    <html {...getHtmlAttributes({ defaultColorScheme, lang })}>
+      <head>
+        <StartDsfr />
+        <DsfrHead Link={Link} />
+      </head>
+      <body>
+        <DsfrProvider lang={lang}>{children}</DsfrProvider>
+      </body>
     </html>
   );
 }
--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,3 +1,11 @@
+import Button from "@codegouvfr/react-dsfr/Button";
+
 export default function Home() {
-  return <main> home page </main>;
+  return (
+    <main>
+      <p>home page (Marianne Font)</p>
+      <Button>DSFR button</Button>
+
+    </main>
+  );
 }
