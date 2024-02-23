a7283021c49c7cfcbfc87ca6c19e96ddbf2bc276
Julien Parmentier <julien.parmentier@pathtech.fr>
2024-02-23 10:53:30 +0100

    chore: logs

--- .ci/test-erb-nginx-conf.sh
+++ .ci/test-erb-nginx-conf.sh
@@ -12,19 +12,17 @@ cd $(dirname $0)
 nginx_servers_erb="servers.conf.erb"
 nginx_servers_conf="$(basename $nginx_servers_erb .erb)"
 
-echo "# test $nginx_servers_erb erb syntax"
+echo "Testing $nginx_servers_erb erb syntax"
 if [ ! -f "$nginx_servers_erb" ] ; then
     echo "ERROR: $nginx_servers_erb not found"
     exit 1
 fi
 ( erb -P -x -T '-' $nginx_servers_erb | ruby -c ) || exit $?
 
-echo "# generate nginx $nginx_servers_conf file"
-# test nginx syntax
-export PORT=${PORT:-80}
+echo "Generating nginx $nginx_servers_conf file"
 erb $nginx_servers_erb > /etc/nginx/conf.d/$nginx_servers_conf
 
-echo "# test nginx $nginx_servers_conf syntax"
+echo "Testing nginx $nginx_servers_conf syntax"
 nginx -t -c /etc/nginx/nginx.conf 2>&1 || exit $?
 
 echo "Test OK"
