2db9e09973d2be9eb66a2ac0e057ed35a33f60aa
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-03-15 16:29:38 +0100

    WIP hero section

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -6,15 +6,25 @@ import PresentationTile from '../components/homepage/PresentationTile';
 import Video from '../components/homepage/Video';
 import Callouts from '../components/homepage/Callouts';
 import News from '../components/homepage/News';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main>
-      <Image src={passSportImage} alt="" />
+      <section className={styles.hero}>
+        <div className={styles.hero_background}>
+          <div className={styles.hero_background_blue}></div>
+        </div>
+        <div className={styles.hero_container}>
+          <Image src={passSportImage} alt="" />
 
-      <h1>Testez votre éligibilité</h1>
-      <p>50€ pour aider les jeunes à faire du sport entre 6 et 30ans</p>
-      <EmailForm />
+          <h1 className={styles.hero_color}>Testez votre éligibilité</h1>
+          <p className={styles.hero_color}>
+            50€ pour aider les jeunes à faire du sport entre 6 et 30ans
+          </p>
+          <EmailForm />
+        </div>
+      </section>
 
       <div className="fr-container">
         <div className="fr-grid-row fr-grid-row--gutters">
