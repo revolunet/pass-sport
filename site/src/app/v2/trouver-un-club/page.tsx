ea4975776bd0b63b1eb03587614500e6bd631c14
Julien Parmentier <julien.parmentier@pathtech.fr>
2024-03-27 16:49:48 +0100

    Revert "style(site): center on small screen"
    This reverts commit 0a786b16e2b9bf14d3a4ca93402aacec91e106a1.

--- site/src/app/v2/trouver-un-club/page.tsx
+++ site/src/app/v2/trouver-un-club/page.tsx
@@ -28,7 +28,7 @@ export default async function TrouverUnClub() {
     <div>
       <TitleHeader
         title="Trouver un club adhérent"
-        subtitle={'Plus de ' + clubs_response.total_count + '  clubs labelisés'}
+        subtitle={'Plus de ' + clubs_response.total_count + '  clubs labelisés trouvés'}
       ></TitleHeader>
       <div className={style.wrapper}>
         <div className={style.container}>
