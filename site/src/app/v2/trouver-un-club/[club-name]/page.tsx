28aa471df7720fa5afb7f2727a6c3d57a8387821
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-04-08 08:35:05 +0200

    show basic info only

---
+++ site/src/app/v2/trouver-un-club/[club-name]/page.tsx
@@ -0,0 +1,172 @@
+import Button from '@codegouvfr/react-dsfr/Button';
+import styles from './style.module.scss';
+import { Tag } from '@codegouvfr/react-dsfr/Tag';
+
+const ClubPage = () => {
+  // const club = {
+  //   nom: 'BOXE PIEDS POINGS CORBEHEM 62',
+  //   cplt_1: 'mairie',
+  //   num_voie: null,
+  //   type_voie: null,
+  //   voie: null,
+  //   bp: null,
+  //   cedex: null,
+  //   cp: '62112',
+  //   commune: 'CORBEHEM',
+  //   telephone: null,
+  //   courriel: null,
+  //   site_web: null,
+  //   est_volontaire: 'Oui',
+  //   activites: ['Kickboxing'],
+  //   a_accueil_handicap_mental: null,
+  //   // a_accueil_handicap_moteur: null,
+  //   a_accueil_handicap_moteur: true,
+
+  //   a_reseau: 'Oui',
+  //   a_agrement: 'Non',
+  //   adresse: 'mairie',
+  //   handicap: 'Non',
+  //   insee_com: '62383/62240',
+  //   com_code: '62240',
+  //   dep_code: '62',
+  //   com_arm_name: 'Corbehem',
+  //   epci_code: 200044048,
+  //   epci_name: 'CC Osartis Marquion',
+  //   dep_name: 'Pas-de-Calais',
+  //   reg_code: 32,
+  //   reg_name: 'Hauts-de-France',
+  //   geoloc_finale: {
+  //     lon: 3.051925,
+  //     lat: 50.335714,
+  //   },
+  // };
+
+  const club = {
+    nom: 'VERTIGE',
+    cplt_1: 'Espace sportif Boris DIAW',
+    num_voie: '11',
+    type_voie: 'RUE',
+    voie: 'de la Renaissance',
+    bp: null,
+    cedex: null,
+    cp: '33400',
+    commune: 'TALENCE',
+    telephone: '0603168266',
+    courriel: null,
+    site_web: null,
+    est_volontaire: 'Oui',
+    activites: [
+      'Autres Arts mariaux et sport de combat (ex: Kendo)',
+      "Course d'orientation",
+      'Randonnée pédestre',
+      'Ski alpinisme',
+      'Ski alpin',
+      'Escalade',
+    ],
+    a_accueil_handicap_mental: null,
+    a_accueil_handicap_moteur: true,
+    a_reseau: 'Oui',
+    a_agrement: 'Oui',
+    adresse: '11 rue de la renaissance',
+    handicap: 'Non',
+    insee_com: '33522',
+    com_code: '33522',
+    dep_code: '33',
+    com_arm_name: 'Talence',
+    epci_code: 243300316,
+    epci_name: 'Bordeaux Métropole',
+    dep_name: 'Gironde',
+    reg_code: 75,
+    reg_name: 'Nouvelle-Aquitaine',
+    geoloc_finale: {
+      lon: -0.594225,
+      lat: 44.818061,
+    },
+  };
+
+  return (
+    <div>
+      <div className={styles.frame}>
+        <section className={styles.info}>
+          <h2>{club.nom}</h2>
+
+          <div className={styles.tags}>
+            <Tag small>
+              <p className="fr-text--xs">{club.activites.length} activités</p>
+            </Tag>
+            {club.a_accueil_handicap_moteur && (
+              <Tag className={styles.disability} small>
+                <p className="fr-text--xs">Accueil personnes aux Handicaps moteurs</p>
+              </Tag>
+            )}
+          </div>
+
+          <div className={styles.contact}>
+            {club.adresse && club.commune && (
+              <p className="fr-text--xs">
+                <span
+                  className={`fr-icon-map-pin-2-line ${styles['icon-color']} fr-icon--sm`}
+                  aria-hidden="true"
+                ></span>
+                {club.adresse}, {club.commune}
+              </p>
+            )}
+
+            {club.telephone && (
+              <p className="fr-text--xs">
+                <span
+                  className={`fr-icon-phone-line ${styles['icon-color']} fr-icon--sm`}
+                  aria-hidden="true"
+                ></span>
+                {club.telephone}
+              </p>
+            )}
+
+            {club.courriel && (
+              <p className="fr-text--xs">
+                <span
+                  className={`fr-icon-mail-line ${styles['icon-color']} fr-icon--sm`}
+                  aria-hidden="true"
+                ></span>
+                {club.courriel}
+              </p>
+            )}
+          </div>
+          <hr className={styles.separator} />
+
+          <h3>Les activités</h3>
+          <ul className={styles.activities}>
+            <div className={styles.grid}>
+              {club.activites.map((activity) => (
+                <li key={activity}>{activity}</li>
+              ))}
+            </div>
+          </ul>
+
+          <hr className={styles.separator} />
+        </section>
+
+        <h4>Où nous trouver</h4>
+      </div>
+
+      <div className={styles['medical-certificate']}>
+        <div className={styles['medical-certificate_container']}>
+          <div className={styles['medical-certificate_container_text-container']}>
+            <h3 className={styles['text-color']}>Besoin d &apos;un certificat médical ?</h3>
+            <p className={`fr-text--lg ${styles['text-color']} ${styles['paragraph']}`}>
+              Ce simulateur vous indique si vous devez obtenir un certificat médical pour pratiquer
+              une activité sportive (loisir ou compétition).
+            </p>
+          </div>
+          <div className={styles['medical-certificate_container_button-wrapper']}>
+            <Button priority="tertiary" size="large">
+              Faire le simulateur de certificat médical
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClubPage;
