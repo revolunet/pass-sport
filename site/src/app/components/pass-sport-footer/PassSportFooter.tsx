4b47d4a7b100df28bb3ffc95bf3255dfac94a71b
Patrick Nguyen <patrick.nguyen2011@gmail.com>
2024-04-19 13:21:53 +0200

    chore: added missing logos + replaced pngs to svgs

--- site/src/app/components/pass-sport-footer/PassSportFooter.tsx
+++ site/src/app/components/pass-sport-footer/PassSportFooter.tsx
@@ -1,29 +1,44 @@
 import Footer, { type FooterProps } from '@codegouvfr/react-dsfr/Footer';
 import { FOOTER_BRAND_TOP } from '@/app/constants/footer-brand-top';
 import styles from './styles.module.scss';
-import defaultLogo from '../../../../public/default-logo.svg';
+import cnosfLogo from '../../../../public/images/footer/cnosf-logo.svg';
+import franceParalympiqueLogo from '../../../../public/images/footer/france-paralympique.svg';
+import menjLogo from '../../../../public/images/footer/menj-logo.svg';
+import ministereSociauxDinumLogo from '../../../../public/images/footer/ministere-sociaux-dinum-logo.svg';
+import passSportLogo from '../../../../public/images/footer/pass-sport-logo.svg';
 
 export default function PassSportFooter() {
   const partnersLogos: FooterProps.PartnersLogos = {
-    // todo: update main with missing image, ask perrine for transparent logo
     main: {
-      linkProps: { title: '', href: '#' },
-      imgUrl: defaultLogo.src,
-      alt: `Ministere de l'education nationale et de la jeune, liberté, égalité, fraternité`,
+      linkProps: { title: `Ministere de l'education nationale et de la jeune, liberté, égalité, fraternité`, href: 'https://www.education.gouv.fr/' },
+      // non-transparent logo
+      imgUrl: menjLogo.src,
+      alt: `Ministere de l'education nationale et de la jeunesse, liberté, égalité, fraternité`,
     },
-    sub: [{
-      linkProps: { title: '', href: '#' },
-      imgUrl: '/images/footer/france-paralympique.png',
-      alt: 'France paralympique',
-    },
-      // todo: add other sub partners logos, ask perrine for transparent logos
+    sub: [
+      {
+        linkProps: { title: 'Comité National Olympique et Sportif Français', href: 'https://cnosf.franceolympique.com/' },
+        imgUrl: cnosfLogo.src,
+        alt: 'Comité National Olympique et Sportif Français',
+      },
+      {
+        linkProps: { title: 'France paralympique', href: 'https://france-paralympique.fr/' },
+        imgUrl: franceParalympiqueLogo.src,
+        alt: 'France paralympique',
+      },
+      {
+        linkProps: { title: 'Ministères sociaux - Secrétariat général Direction du numérique', href: 'https://www.numerique.gouv.fr/dinum/' },
+        // non-transparent logo
+        imgUrl: ministereSociauxDinumLogo.src,
+        alt: 'Ministères sociaux - Secrétariat général Direction du numérique',
+      },
     ]
   };
 
-  const homeLinkProps: NonNullable<FooterProps['homeLinkProps']> = { title: 'Home', href: '/v2/accueil' }
+  const homeLinkProps: NonNullable<FooterProps['homeLinkProps']> = { title: `Pass'Sport accueil`, href: '/v2/accueil' }
   const operatorLogo: NonNullable<FooterProps['operatorLogo']> = {
     orientation: 'horizontal',
-    imgUrl: '/images/footer/pass-sport-logo.png',
+    imgUrl: passSportLogo.src,
     alt: `Pass'Sport`,
   };
 
