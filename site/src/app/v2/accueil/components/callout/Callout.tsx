dfec80104282b845c612b805d698c6b7ea08fa06
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-04-25 11:28:01 +0200

    use dsfr spacing

--- site/src/app/v2/accueil/components/callout/Callout.tsx
+++ site/src/app/v2/accueil/components/callout/Callout.tsx
@@ -7,8 +7,8 @@ const Callouts = () => {
       <div className="fr-grid-row">
         {calloutContents.map((content) => {
           return (
-            <div key={content.id} className={`fr-col-12 fr-col-lg-3 ${styles.cells}`}>
-              <div className={`fr-callout ${styles.content}`}>
+            <div key={content.id} className={`fr-col-12 fr-col-lg-3 fr-py-2w ${styles.background}`}>
+              <div className={`fr-callout fr-mb-0 ${styles.background}`}>
                 <h3 className="fr-callout__title">{content.title}</h3>
                 <p className="fr-callout__text">{content.description}</p>
               </div>
