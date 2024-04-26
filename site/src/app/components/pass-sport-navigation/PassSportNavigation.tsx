f348c4288d5615e29ab448a41b03f57817f1b40a
Julien Parmentier <julien.parmentier@pathtech.fr>
2024-04-26 10:25:29 +0200

    fix(site): fix active link for question, refactor with list

--- site/src/app/components/pass-sport-navigation/PassSportNavigation.tsx
+++ site/src/app/components/pass-sport-navigation/PassSportNavigation.tsx
@@ -2,6 +2,7 @@
 import { FOOTER_BRAND_TOP } from '@/app/constants/footer-brand-top';
 import Header from '@codegouvfr/react-dsfr/Header';
 import { usePathname } from 'next/navigation';
+import { navigationItem } from './navigation';
 
 export default function PassSportNavigation() {
   const paths: string | null = usePathname();
@@ -17,38 +18,14 @@ export default function PassSportNavigation() {
           href: '/v2/accueil',
           title: "Accueil - Nom de l'entité (ministère, secrétariat d'état, gouvernement)",
         }}
-        navigation={[
-          {
-            isActive: isActive('/v2/accueil'),
-            linkProps: {
-              href: '/v2/accueil',
-              target: '_self',
-            },
-            text: 'Accueil',
+        navigation={navigationItem.map((i) => ({
+          isActive: isActive(i.link),
+          linkProps: {
+            href: i.link,
+            target: '_self',
           },
-          {
-            isActive: isActive('/v2/tout-savoir-sur-le-pass-sport'),
-            text: 'Tout savoir sur le Pass Sport',
-            linkProps: {
-              href: '/v2/tout-savoir-sur-le-pass-sport',
-            },
-          },
-          {
-            linkProps: {
-              href: '/v2/une-question',
-              target: '_self',
-            },
-            text: 'Une question ?',
-          },
-          {
-            isActive: isActive('/v2/trouver-un-club'),
-            linkProps: {
-              href: '/v2/trouver-un-club',
-              target: '_self',
-            },
-            text: 'Trouver un club',
-          },
-        ]}
+          text: i.text,
+        }))}
       />
     </div>
   );
---
+++ site/src/app/components/pass-sport-navigation/navigation.ts
@@ -0,0 +1,14 @@
+type NavigationItem = {
+  link: string;
+  text: string;
+};
+
+export const navigationItem: NavigationItem[] = [
+  {
+    link: '/v2/accueil',
+    text: 'Accueil',
+  },
+  { link: '/v2/tout-savoir-sur-le-pass-sport', text: 'Tout savoir sur le Pass Sport' },
+  { link: '/v2/une-question', text: 'Une question ?' },
+  { link: '/v2/trouver-un-club', text: 'Trouver un club' },
+];
