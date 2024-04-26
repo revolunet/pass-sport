1e9f611f3d6c0ef81fd7e39df796e964ab3c6943
Julien Parmentier <julien.parmentier@pathtech.fr>
2024-04-26 10:37:59 +0200

    fix(site): navigation as tsx

---
+++ site/src/app/components/pass-sport-navigation/navigation.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+type NavigationItem = {
+  link: string;
+  text: string | JSX.Element;
+};
+
+export const navigationItem: NavigationItem[] = [
+  {
+    link: '/v2/accueil',
+    text: (
+      <>
+        <span className="fr-pr-1w ri-home-line" aria-hidden="true"></span>
+        Accueil
+      </>
+    ),
+  },
+  {
+    link: '/v2/tout-savoir-sur-le-pass-sport',
+    text: (
+      <div className={styles['menu-item-spacer']}>
+        <span aria-hidden="true"></span>
+        Tout savoir sur le pass Sport
+      </div>
+    ),
+  },
+  { link: '/v2/trouver-un-club', text: 'Trouver un club' },
+  { link: '/v2/une-question', text: 'Une question ?' },
+];
