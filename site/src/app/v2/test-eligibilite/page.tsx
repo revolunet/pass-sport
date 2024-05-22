2af8c7266f0e3f8c9c4a6ce286b16dc9088fe842
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-05-22 15:45:38 +0200

    improve padding

--- site/src/app/v2/test-eligibilite/page.tsx
+++ site/src/app/v2/test-eligibilite/page.tsx
@@ -9,13 +9,13 @@ export const metadata: Metadata = {
 
 const EligibilityTest = () => {
   return (
-    <main className={styles.main}>
+    <main className={cn('fr-pb-4w', styles.main)}>
       <h1 className={cn('fr-pt-2w', 'fr-pb-3w', 'fr-px-2w', styles.title)}>
         Puis-je bénéficier du pass Sport?
       </h1>
 
-      <div className={cn('fr-mb-8w', 'fr-mx-auto', 'fr-px-2w', styles.background)}>
-        <div className={cn('fr-pt-7w', 'fr-pb-0', 'fr-mx-auto', styles.wrapper)}>
+      <div className={cn('fr-pb-4w', 'fr-mx-auto', 'fr-px-2w', styles.background)}>
+        <div className={cn('fr-pt-7w', 'fr-mx-auto', styles.wrapper)}>
           <AllowanceStep />
         </div>
       </div>
