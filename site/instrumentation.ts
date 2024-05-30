2110fdb2ab7240d0dee0d39cfa841bb4d1a2f695
Julien Parmentier <julien.parmentier@pathtech.fr>
2024-05-30 10:48:13 +0200

    fix(site): prevent sentry init for test

--- site/instrumentation.ts
+++ site/instrumentation.ts
@@ -1,15 +1,13 @@
-// instrumentation.ts
-
 import * as Sentry from '@sentry/nextjs';
 
 export function register() {
-  if (!!process.env.SENTRY_DSN) {
+  // Sentry configuration for SSR (Nodejs)
+  if (!!process.env.SENTRY_DSN && !!process.env.ENV) {
     Sentry.init({
       environment: process.env.ENV,
       dsn: process.env.SENTRY_DSN,
       tracesSampleRate: 1.0,
       attachStacktrace: true,
-      // Your Node.js Sentry configuration...
     });
     console.info('Sentry initialized for ' + process.env.NEXT_RUNTIME);
   }
--- site/sentry.client.config.ts
+++ site/sentry.client.config.ts
@@ -1,9 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
 
 if (!!process.env.SENTRY_DSN) {
+  // Sentry configuration for browser
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     environment: process.env.ENV,
-    // Your Sentry configuration for the Browser...
   });
 }
