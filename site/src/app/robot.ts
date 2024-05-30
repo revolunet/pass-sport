01e4610094211911daa6350c442dffa102008fa1
Julien Parmentier <julien.parmentier@pathtech.fr>
2024-05-30 18:21:27 +0200

    fix(site): do not index qr code page

--- site/src/app/robot.ts
+++ site/src/app/robot.ts
@@ -4,13 +4,9 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: 'Googlebot',
+        userAgent: '*',
         allow: ['/'],
-        disallow: '/private/',
-      },
-      {
-        userAgent: ['Applebot', 'Bingbot'],
-        disallow: ['/'],
+        disallow: '/v2/code/scan/',
       },
     ],
     sitemap: 'https://pass.sports.gouv.fr/sitemap.xml',
