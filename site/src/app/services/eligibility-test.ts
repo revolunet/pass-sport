d4e57ef424214429a6895c37e53efab5b0c2e77d
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-06-03 11:35:58 +0200

    fetch both versions for each route

--- site/src/app/services/eligibility-test.ts
+++ site/src/app/services/eligibility-test.ts
@@ -10,7 +10,7 @@ import {
 import * as Sentry from '@sentry/nextjs';
 import { addQrCodeToConfirmResponse } from './qr-code';
 
-export const buildLCAConfirmUrl = (data: ConfirmPayload): URL => {
+export const buildLCAConfirmUrl = (data: ConfirmPayload, isUsingApiV1: boolean): URL => {
   const domain = process.env.LCA_API_URL;
 
   if (!domain) {
@@ -52,14 +52,17 @@ export const buildLCAConfirmUrl = (data: ConfirmPayload): URL => {
     params.append('codeIso', recipientBirthCountry);
   }
 
-  const baseUrl = `${domain}/gw/psp-server/beneficiaires/confirm`;
+  const baseUrl = isUsingApiV1
+    ? `${domain}/gw/psp-server/beneficiaires/confirm`
+    : `${domain}/apim/api-asso-admin/passsport/beneficiaires/confirm`;
+
   const url = new URL(baseUrl);
   url.search = params.toString();
 
   return url;
 };
 
-export const buildLCASearchUrl = (data: SearchPayload): URL => {
+export const buildLCASearchUrl = (data: SearchPayload, isUsingApiV1: boolean): URL => {
   const domain = process.env.LCA_API_URL;
 
   if (!domain) {
@@ -72,7 +75,10 @@ export const buildLCASearchUrl = (data: SearchPayload): URL => {
   params.append('dateNaissance', data.beneficiaryBirthDate);
   params.append('codeInsee', data.recipientResidencePlace);
 
-  const baseUrl = `${domain}/gw/psp-server/beneficiaires/search`;
+  const baseUrl = isUsingApiV1
+    ? `${domain}/gw/psp-server/beneficiaires/search`
+    : `${domain}/apim/api-asso-admin/passsport/beneficiaires/search`;
+
   const url = new URL(baseUrl);
   url.search = params.toString();
 
@@ -82,15 +88,27 @@ export const buildLCASearchUrl = (data: SearchPayload): URL => {
 export const fetchQrCode = async (
   payload: ConfirmPayload,
 ): Promise<ConfirmResponseBody | ConfirmResponseErrorBody> => {
-  const url: URL = buildLCAConfirmUrl(payload);
+  const authenticationKey = process.env.LCA_API_KEY;
 
-  const response = await fetch(url);
+  if (!authenticationKey) {
+    throw new Error('LCA authentication key is missing');
+  }
 
-  if (!response.ok) {
+  const url: URL = buildLCAConfirmUrl(payload, true);
+  const url2 = buildLCAConfirmUrl(payload, false);
+
+  const [response1, response2] = await Promise.all([
+    fetch(url),
+    fetch(url2, { headers: { 'X-Gravitee-Api-Key': authenticationKey } }),
+  ]);
+
+  if (!response2.ok && !response1.ok) {
     throw new Error(
-      `Request to LCA api on /confirm has failed. Response status is ${response.status}. Response body is ${JSON.stringify(await response.json())}`,
+      `Request to LCA api on /confirm has failed. API V1: Response status is ${response1.status}; Response body is ${JSON.stringify(await response1.json())}. API V2: Response status is ${response2.status}; Response body is ${JSON.stringify(await response2.json())}`,
     );
   }
+
+  const response = !response2.ok ? response1 : response2;
   const responseBody = (await response.json()) as ConfirmResponseBody | ConfirmResponseErrorBody;
 
   if ('message' in responseBody) {
@@ -109,15 +127,27 @@ export const fetchQrCode = async (
 };
 
 export const fetchEligible = async (payload: SearchPayload) => {
-  const url: URL = buildLCASearchUrl(payload);
+  const authenticationKey = process.env.LCA_API_KEY;
 
-  const response = await fetch(url);
+  if (!authenticationKey) {
+    throw new Error('LCA authentication key is missing');
+  }
 
-  if (!response.ok) {
+  const url: URL = buildLCASearchUrl(payload, true);
+  const url2: URL = buildLCASearchUrl(payload, false);
+
+  const [response1, response2] = await Promise.all([
+    fetch(url),
+    fetch(url2, { headers: { 'X-Gravitee-Api-Key': authenticationKey } }),
+  ]);
+
+  if (!response2.ok && !response1.ok) {
     throw new Error(
-      `Request to LCA api on /search has failed. Response status is ${response.status}. Response body is ${JSON.stringify(await response.json())}`,
+      `Request to LCA api on /search has failed. API V1: Response status is ${response1.status}; Response body is ${JSON.stringify(await response1.json())}. API V2: Response status is ${response2.status}; Response body is ${JSON.stringify(await response2.json())}`,
     );
   }
+
+  const response = !response2.ok ? response1 : response2;
   const responseBody = (await response.json()) as SearchResponseBody | SearchResponseErrorBody;
 
   if ('message' in responseBody) {
