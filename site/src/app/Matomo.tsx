f38a6d50040f7a9978184c5785931f3866d989a3
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-06-14 14:45:02 +0200

    remove double tracking; fix sending qrcode segment to matomo

--- site/src/app/Matomo.tsx
+++ site/src/app/Matomo.tsx
@@ -1,32 +1,12 @@
 'use client';
 import init, { push } from '@socialgouv/matomo-next';
 import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
-
-let IS_MATOMO_INITIALIZED: boolean = false;
+import { useEffect, useRef, useState } from 'react';
 
 export default function Matomo() {
-  // Matomo init
-  useEffect(() => {
-    if (!IS_MATOMO_INITIALIZED) {
-      init({
-        url: process.env.NEXT_PUBLIC_MATOMO_URL || '',
-        siteId: process.env.NEXT_PUBLIC_MATOMO_SITE_ID || '',
-        // excludeUrlsPatterns: [/^\//],
-      });
-    }
-    IS_MATOMO_INITIALIZED = true;
-  }, []);
+  const isInitialLoad = useRef(true);
 
-  const location = usePathname();
-  const [previousURL, setPreviousURL] = useState<string>();
-
-  // track basic navigation
-  useEffect(() => {
-    if (previousURL) {
-      push(['setReferrerUrl', previousURL]);
-    }
-
+  const transformQrCodeUrl = (): string => {
     const { pathname } = window.location;
 
     let updatedPathName = pathname;
@@ -35,11 +15,39 @@ export default function Matomo() {
     if (regex.test(pathname)) {
       updatedPathName = pathname.replace(regex, `/code/scan/24-xxxx-xxxx`);
     }
+    return updatedPathName;
+  };
 
-    push(['setCustomUrl', updatedPathName]);
-    push(['trackPageView']);
+  // Initialize matomo. init() will also send the current page viewed to matomo server
+  useEffect(() => {
+    init({
+      url: process.env.NEXT_PUBLIC_MATOMO_URL || '',
+      siteId: process.env.NEXT_PUBLIC_MATOMO_SITE_ID || '',
+      onInitialization: () => {
+        push(['setCustomUrl', transformQrCodeUrl()]);
+      },
+    });
+  }, []);
 
-    setPreviousURL(updatedPathName);
+  const location = usePathname();
+  const [previousURL, setPreviousURL] = useState<string>();
+
+  // Track navigation from one page to another for SPA
+  useEffect(() => {
+    if (isInitialLoad.current) {
+      isInitialLoad.current = false;
+    } else {
+      if (previousURL) {
+        push(['setReferrerUrl', previousURL]);
+      }
+
+      let updatedPathName = transformQrCodeUrl();
+      push(['setCustomUrl', updatedPathName]);
+      push(['trackPageView']);
+
+      setPreviousURL(updatedPathName);
+    }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
