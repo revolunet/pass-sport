faf0db0b7a440fb1e560945e8a6734ebde0f55e7
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-07-12 14:42:28 +0200

    use lists for club tags and contact details; refacto

--- site/src/app/v2/trouver-un-club/[club-name]/page.tsx
+++ site/src/app/v2/trouver-un-club/[club-name]/page.tsx
@@ -2,6 +2,7 @@ import ClubDetails from '@/app/v2/trouver-un-club/[club-name]/components/clubDet
 import EligibilityTestBanner from '../../../../../components/eligibility-test-banner/EligibilityTestBanner';
 import SocialMediaPanel from '../../../components/social-media-panel/SocialMediaPanel';
 import { Metadata } from 'next';
+import { SKIP_LINKS_ID } from '@/app/constants/skip-links';
 
 interface Props {
   params: { 'club-name': string };
@@ -17,11 +18,11 @@ const ClubPage = ({ params }: { params: { 'club-name': string } }) => {
   const clubName = decodeURIComponent(params['club-name']);
 
   return (
-    <>
+    <main tabIndex={-1} id={SKIP_LINKS_ID.mainContent} role="main">
       <ClubDetails clubName={clubName} />
       <EligibilityTestBanner />
       <SocialMediaPanel />
-    </>
+    </main>
   );
 };
 
