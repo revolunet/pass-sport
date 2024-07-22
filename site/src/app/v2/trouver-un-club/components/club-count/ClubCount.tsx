efda20966b53ca9df2d20f11032d20c8a2b939af
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-07-22 13:26:36 +0200

    inform users about club counts changes

--- site/src/app/v2/trouver-un-club/components/club-count/ClubCount.tsx
+++ site/src/app/v2/trouver-un-club/components/club-count/ClubCount.tsx
@@ -9,27 +9,28 @@ interface Props {
 }
 
 const ClubCount: React.FC<Props> = ({ displayedClubCount, totalClubCount, isPaginating }) => {
-  if (isPaginating) {
-    return (
-      <p className={cn('fr-text--lead', 'fr-mb-0', styles.paragraph)}>
-        {displayedClubCount} {displayedClubCount > 1 ? 'clubs affichés' : 'club affiché'} sur{' '}
-        {totalClubCount} {totalClubCount > 1 ? 'clubs référencés' : 'club référencé'}
-      </p>
-    );
-  }
+  const buildContent = () => {
+    if (isPaginating) {
+      return `${displayedClubCount} ${displayedClubCount > 1 ? 'clubs affichés' : 'club affiché'} sur 
+    ${totalClubCount} ${totalClubCount > 1 ? 'clubs référencés' : 'club référencé'}`;
+    }
 
-  if (displayedClubCount >= LIMIT) {
-    return (
-      <p className={cn('fr-text--lead', 'fr-mb-0', styles.paragraph)}>
-        Les clubs trouvés sont trop nombreux. Seuls les {LIMIT} premiers sont affichés. Merci
-        d&apos;affiner votre recherche.
-      </p>
-    );
-  }
+    if (displayedClubCount >= LIMIT) {
+      return `Les clubs trouvés sont trop nombreux. Seuls les ${LIMIT} premiers sont affichés. Merci
+      d&apos;affiner votre recherche.`;
+    }
+
+    return `${displayedClubCount} ${displayedClubCount > 1 ? 'clubs référencés' : 'club référencé'}`;
+  };
 
   return (
-    <p className={cn('fr-text--lead', 'fr-mb-0', styles.paragraph)}>
-      {displayedClubCount} {displayedClubCount > 1 ? 'clubs référencés' : 'club référencé'}
+    <p
+      role="status"
+      aria-live="polite"
+      aria-atomic={true}
+      className={cn('fr-text--lead', 'fr-mb-0', styles.paragraph)}
+    >
+      {buildContent()}
     </p>
   );
 };
