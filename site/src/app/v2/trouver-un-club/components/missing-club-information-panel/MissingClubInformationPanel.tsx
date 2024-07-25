6a77a5b4d49ec4b553664ce2d7f993803b8f0988
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-07-25 15:25:36 +0200

    update words, update information panel when clubs are missing on pro page

--- site/src/app/v2/trouver-un-club/components/missing-club-information-panel/MissingClubInformationPanel.tsx
+++ site/src/app/v2/trouver-un-club/components/missing-club-information-panel/MissingClubInformationPanel.tsx
@@ -1,23 +1,38 @@
 import cn from 'classnames';
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 interface Props {
   isProVersion?: boolean;
 }
 
 const MissingClubInformationPanel = ({ isProVersion }: Props) => {
-  return (
-    <div
-      className={cn('fr-alert', 'fr-alert--info', 'fr-mt-9w', 'fr-mx-auto', styles['alert-sizer'])}
-    >
-      <h6 className="fr-alert__title">Information</h6>
+  const proContent = (
+    <>
       <p>
-        {isProVersion
-          ? `Si votre club n'apparait pas, c'est qu'il n'est pas encore référencé. Dans ce cas là, n'hésitez pas à vous rapprocher de plusieurs interlocuteurs dans votre département en fonction de votre statut`
-          : `Si mon club n’apparait pas, c’est qu’il n’accepte probablement pas encore le pass
-        Sport. N’hésitez pas à vous rapprocher de votre club en lui proposant d’accepter le
-        dispositif.`}
+        Votre structure n&apos;apparait pas ?{' '}
+        <Link href="/v2/pro/tout-savoir-sur-le-pass-sport#tuto-carto" target="_blank">
+          voici comment la cartographier.
+        </Link>
       </p>
+      <p>
+        N&apos;hésitez pas à vous rapprocher de plusieurs interlocuteurs dans votre département en
+        fonction de votre statut.
+      </p>
+    </>
+  );
+  return (
+    <div className={cn('fr-alert', 'fr-alert--info', 'fr-mx-auto', styles['alert-sizer'])}>
+      <h6 className="fr-alert__title">Information</h6>
+
+      {isProVersion ? (
+        proContent
+      ) : (
+        <p>
+          Si mon club n’apparait pas, c’est qu’il n’accepte probablement pas encore le pass Sport.
+          N’hésitez pas à vous rapprocher de votre club en lui proposant d’accepter le dispositif.
+        </p>
+      )}
     </div>
   );
 };
