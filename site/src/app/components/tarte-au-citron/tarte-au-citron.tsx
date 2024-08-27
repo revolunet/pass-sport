4109d2c0fb28c209b82632b732425937301c708c
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-08-27 12:01:16 +0200

    variabilise tarteaucitron domain

--- site/src/app/components/tarte-au-citron/tarte-au-citron.tsx
+++ site/src/app/components/tarte-au-citron/tarte-au-citron.tsx
@@ -3,9 +3,15 @@
 import Script from 'next/script';
 
 export const TarteAuCitron = () => {
+  const domain = process.env.NEXT_PUBLIC_TARTEAUCITRON_DOMAIN;
+  if (!domain) {
+    console.error('Environment variable NEXT_PUBLIC_TARTEAUCITRON_DOMAIN is missing');
+    return null;
+  }
+
   return (
     <Script
-      src="https://tarteaucitron.io/load.js?domain=localhost%2Fv2&uuid=19b13211bfb1bd1efd6f804a26674ed864265114"
+      src={`https://tarteaucitron.io/load.js?domain=${domain}&uuid=19b13211bfb1bd1efd6f804a26674ed864265114`}
       strategy="beforeInteractive"
     />
   );
