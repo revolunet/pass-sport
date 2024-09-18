e657fdbf3f54b0ae99f5078af5dbc89b25d4f27a
Maxime Chaillet <maxime.chaillet@pathtech.fr>
2024-09-18 13:58:07 +0200

    restore csp

--- site/middleware.ts
+++ site/middleware.ts
@@ -8,24 +8,23 @@ export function middleware(request: NextRequest) {
       ? `'self' 'nonce-${nonce}' 'strict-dynamic'`
       : `'self' 'unsafe-inline' 'unsafe-eval'`;
 
-      const cspHeader =''
-//   const cspHeader = `
-//     default-src 'self';
-//     script-src 'report-sample' ${scriptSrc} https://client.crisp.chat/ https://stats.beta.gouv.fr/matomo.js https://tarteaucitron.io/ https://cdn.tarteaucitron.io/;
-//     style-src 'report-sample' 'unsafe-inline' 'self' https://unpkg.com https://client.crisp.chat/ https://cdn.tarteaucitron.io/;
-//     object-src 'none';
-//     base-uri 'self';
-//     form-action 'self';
-//     connect-src 'self' https://client.crisp.chat/ wss://client.relay.crisp.chat/ https://sports-sgsocialgouv.opendatasoft.com https://stats.beta.gouv.fr https://geo.api.gouv.fr;
-//     font-src 'self' https://client.crisp.chat/;
-//     frame-src 'self' https://player.vimeo.com https://pass-sport.crisp.help/;
-//     img-src 'self' data: https://image.crisp.chat/ https://client.crisp.chat/ https://storage.crisp.chat/ https://a.tile.openstreetmap.org https://b.tile.openstreetmap.org https://c.tile.openstreetmap.org https://i.vimeocdn.com https://unpkg.com https://tarteaucitron.io;
-//     manifest-src 'self';
-//     media-src 'self';
-//     report-uri https://66ab4d8ba05c71ef29160216.endpoint.csper.io/?v=1;
-//     worker-src 'none';
-//     upgrade-insecure-requests;
-// `;
+  const cspHeader = `
+    default-src 'self';
+    script-src 'report-sample' ${scriptSrc} https://client.crisp.chat/ https://stats.beta.gouv.fr/matomo.js https://tarteaucitron.io/ https://cdn.tarteaucitron.io/;
+    style-src 'report-sample' 'unsafe-inline' 'self' https://unpkg.com https://client.crisp.chat/ https://cdn.tarteaucitron.io/;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    connect-src 'self' https://client.crisp.chat/ wss://client.relay.crisp.chat/ https://sports-sgsocialgouv.opendatasoft.com https://stats.beta.gouv.fr https://geo.api.gouv.fr;
+    font-src 'self' https://client.crisp.chat/;
+    frame-src 'self' https://player.vimeo.com https://pass-sport.crisp.help/;
+    img-src 'self' data: https://image.crisp.chat/ https://client.crisp.chat/ https://storage.crisp.chat/ https://a.tile.openstreetmap.org https://b.tile.openstreetmap.org https://c.tile.openstreetmap.org https://i.vimeocdn.com https://unpkg.com https://tarteaucitron.io;
+    manifest-src 'self';
+    media-src 'self';
+    report-uri https://66ab4d8ba05c71ef29160216.endpoint.csper.io/?v=1;
+    worker-src 'none';
+    upgrade-insecure-requests;
+`;
   /**
    ** TODO : add trusted type policies
     require-trusted-types-for 'script';
