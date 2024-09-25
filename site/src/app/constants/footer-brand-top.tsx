02475f96667ffce0e9135875e4834996b60e749e
Patrick Nguyen <patrick.nguyen2011@gmail.com>
2024-09-25 18:53:53 +0200

    chore: wording fix

--- site/src/app/constants/footer-brand-top.tsx
+++ site/src/app/constants/footer-brand-top.tsx
@@ -1,8 +1,8 @@
 export const FOOTER_BRAND_TOP = (
   <>
-    <span className="display--block">Ministère</span>
-    <span className="display--block">des sports</span>
-    <span className="display--block">et des jeux Olympiques</span>
+    <span className="display--block">Ministère </span>
+    <span className="display--block">des sports </span>
+    <span className="display--block">et des jeux Olympiques </span>
     <span className="display--block">et Paralympiques</span>
   </>
 );
