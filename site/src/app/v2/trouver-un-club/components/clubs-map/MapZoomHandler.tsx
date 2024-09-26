e00cdf40ba45e14eb0f107fc45c7c21eed4c9dcd
Patrick Nguyen <patrick.nguyen2011@gmail.com>
2024-09-26 12:35:29 +0200

    chore: voiceover fix macos builtin

--- site/src/app/v2/trouver-un-club/components/clubs-map/MapZoomHandler.tsx
+++ site/src/app/v2/trouver-un-club/components/clubs-map/MapZoomHandler.tsx
@@ -7,23 +7,10 @@ const MapZoomHandler = () => {
   const [msg, setMsg] = useState<string | null>(null);
 
   map.on('zoomend', (e) => {
-    setMsg('le zoom de la carte a changé.');
+    setMsg(`le zoom de la carte est maintenant au niveau ${map.getZoom()}`);
   });
 
-  useEffect(() => {
-    let timer: NodeJS.Timeout | null = null;
-    if (msg) {
-      timer = setTimeout(() => {
-        setMsg(null);
-      }, 1000);
-    }
-
-    return () => {
-      timer && clearTimeout(timer);
-    };
-  }, [msg]);
-
-  return <div aria-live="polite">{msg ? <p>{msg}</p> : null}</div>;
+  return <p aria-live="polite">{msg}</p>;
 };
 
 export default MapZoomHandler;
