505638799a991cb5ca413fb56579f84f7cf785e7
Patrick Nguyen <patrick.nguyen2011@gmail.com>
2024-09-26 14:10:32 +0200

    chore: header logo updated

--- site/src/app/constants/footer-brand-top.tsx
+++ site/src/app/constants/footer-brand-top.tsx
@@ -1,8 +1,8 @@
 export const FOOTER_BRAND_TOP = (
   <>
     <span className="display--block">Ministère </span>
-    <span className="display--block">des sports </span>
-    <span className="display--block">et des jeux Olympiques </span>
-    <span className="display--block">et Paralympiques</span>
+    <span className="display--block">des sports, </span>
+    <span className="display--block">de la jeunesse </span>
+    <span className="display--block">et de la vie associative</span>
   </>
 );
